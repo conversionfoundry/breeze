0e24c02a884d9c9bb7630831aa0f68418ae05bff
Matt Powell <fauxparse@gmail.com>
2010-05-16 09:21:56 +1200

    fix association

--- app/models/breeze/content/mixins/tree_structure.rb
+++ app/models/breeze/content/mixins/tree_structure.rb
@@ -4,7 +4,7 @@ module Breeze
       module TreeStructure
         def self.included(base)
           base.belongs_to_related :parent
-          base.has_many_related :children, :foreign_key => :parent_id
+          base.has_many_related :children, :class_name => base.name, :foreign_key => :parent_id
           base.field :position, :type => Integer, :default => 0
           
           base.named_scope :root, :conditions => { :parent_id => nil }
