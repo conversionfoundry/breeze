6c879ee881ae4618e523d710f1d4286466dff30c
Matt Powell <fauxparse@gmail.com>
2010-06-11 17:30:03 +1200

    menu link to edit themes

--- app/models/breeze/admin/ability.rb
+++ app/models/breeze/admin/ability.rb
@@ -10,6 +10,7 @@ module Breeze
         when User
           can :manage,       Breeze::Content::Item
           can :manage,       Breeze::Content::Custom::Type if user.admin? || user.designer?
+          can :manage,       Breeze::Theming::Theme if user.admin? || user.designer?
           can :update,       Breeze::Admin::User   do |subject| ; user.admin? || user == subject ; end
           can :destroy,      Breeze::Admin::User   do |subject| ; user.admin? && user != subject ; end
           can :assign_roles, Breeze::Admin::User   do |subject| ; user.admin?                    ; end
