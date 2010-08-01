1f04b1a2ba0d737528d0a427cf57824d90910ea3
Matt Powell <fauxparse@gmail.com>
2010-08-02 11:13:51 +1200

    theme generator wasn't creating directories properly

--- lib/generators/breeze/theme/theme_generator.rb
+++ lib/generators/breeze/theme/theme_generator.rb
@@ -5,7 +5,11 @@ module Breeze
     class ThemeGenerator < Rails::Generators::NamedBase
       def create_theme
         in_root do
-          run extify("mkdir -p ./vendor/themes/#{name}/{images,javascripts,layouts,partials,stylesheets}")
+          run extify("mkdir -p ./vendor/themes/#{name}/images")
+          run extify("mkdir -p ./vendor/themes/#{name}/javascripts")
+          run extify("mkdir -p ./vendor/themes/#{name}/layouts")
+          run extify("mkdir -p ./vendor/themes/#{name}/partials")
+          run extify("mkdir -p ./vendor/themes/#{name}/stylesheets")
         end
       end
     end
