02c4d4f4e795b00c96cba2359f31907a7a6f14a2
Matt Powell <fauxparse@gmail.com>
2010-09-13 11:38:33 +1200

    resque queueing fix

--- lib/breeze/queueing/resque.rb
+++ lib/breeze/queueing/resque.rb
@@ -4,9 +4,20 @@ module Breeze
       @queue = :breeze
       
       def self.perform(class_name, id, message, *args)
-        if receiver = class_name.constantize.find(id)
+	      receiver = self.fetch class_name, id
+        if receiver
           receiver.send message, *args
+        else
+          raise "Couldn't process #{[class_name, id, message, *args].inspect}"
         end
+      end
+      
+    protected
+      def self.fetch(class_name, id)
+        id = id.to_s
+        klass = class_name.constantize
+        klass.where(:_id => id).first ||
+        klass.where(:_id => BSON::ObjectID.from_string(id)).first
       end
     end
 
