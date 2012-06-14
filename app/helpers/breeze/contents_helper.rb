9540a8567770079cad471f8f19217c98c0edbd6b
Isaac Freeman <isaac@leftclick.com>
2012-06-15 10:54:30 +1200

    Child permalinks update correctly when a parent slug changes

--- app/helpers/breeze/contents_helper.rb
+++ app/helpers/breeze/contents_helper.rb
@@ -26,6 +26,8 @@ module Breeze
       @_region_contents[name]
     end
     
+    # Navigation Tag
+    # Generates a simple ul structure of pages on the site
     def navigation(*args, &block)
       levels = { :primary => 1, :secondary => 2, :tertiary => 3 }
       options = args.extract_options!
@@ -87,7 +89,7 @@ module Breeze
             end
             
             if recurse > 0 && p.level == level && !p.root?
-              unless (child = page || p.children.first).nil?
+              unless (child = p.children.first).nil?
                 link << navigation(child, options.merge(:level => level + 1, :recurse => recurse - 1), &block)
               end
             end
@@ -97,5 +99,120 @@ module Breeze
       end
       content_tag :ul, contents.html_safe, options.except(:level, :recurse).reverse_merge(:class => "#{levels.invert[level] || "level-#{level}"} navigation")
     end
+        
+    # First stab at a Twitter Bootstrap compatible navigation menu
+    # TODO: Decide whether to do this sort of thing.
+    # Arguments:
+    # :level => [1,2,3] (level one appears on all pages, 2 only on level a pages, 3 on level 2 pages, etc.)
+    # :recurse => [true/false,:active,numeric] (level one appears on all pages, 2 only on level a pages, 3 on level 2 pages, etc.)
+    # :home => [true/false] (include or exclude the home link)
+    def bootstrap_nav(*args, &block)
+      levels = { :primary => 1, :secondary => 2, :tertiary => 3 }
+      options = args.extract_options!
+      level = levels[options[:level]] || (options[:level] || 1).to_i
+
+      # If there are no arguments, use the current page
+      args.unshift page if args.empty? && !page.nil?
+      
+      contents = "".tap do |str|
+        # Opening HTML for Twitter Bootstrap Navigation
+        if level == 1
+          str << '<ul class="nav">'
+        else
+          str << '<ul class="dropdown-menu">'
+        end
+            
+        pages = args.map do |arg|
+          if arg.root?
+            arg.children.first
+          else 
+            case arg
+            when Breeze::Content::NavigationItem then arg
+            else Breeze::Content::NavigationItem.where(:permalink => arg.to_s).first
+            end
+          end
+        end.flatten.compact
+                                        
+        ancestry = pages.first ? pages.first.self_and_ancestors.to_a : [ page ]
+        
+        active = page ? (page.root? ? [page] : ancestry.dup) : []
+        ancestry << ancestry.last.children.first
+        ancestry.compact!
+                        
+        if level <= ancestry.length && ancestry[level].present?
+          siblings = ancestry[level].self_and_siblings.to_a.select(&:show_in_navigation?)          
+          # siblings = page.self_and_siblings.to_a.select(&:show_in_navigation?)          
+          siblings.unshift ancestry[level - 1] if options[:home] || (level == 1 && options[:home] != false)
+          siblings.each_with_index do |p, i|
+            page_title = if (options[:home] && options[:home] != true) && (p.level < level || p.root?)
+              options[:home]
+              case options[:home]
+              when true   then p.title
+              when Symbol then p.send options[:home]
+              when Proc   then options[:home].call(p)
+              else options[:home].to_s
+              end
+            else
+              p.title
+            end
+            page_title = p.title if page_title.blank?
+            
+            link_options = ({}).tap do |o|
+              o[:class] = [ p.root? ? "home" : p.slug ].tap do |classes|
+                classes << "active" if p == page || (active.index(p).to_i > 0 && p.level == level)
+                classes << "first"  if i == 0
+                classes << "last"   if i == siblings.length - 1
+              end.join(" ")
+            end
+            
+            link = if block_given?
+              capture p, link_options, &block
+            else
+              link_to content_tag(:span, "#{page_title}".html_safe), p.permalink, link_options
+            end
+            
+            recurse = case options[:recurse]
+             when true             then 3
+             when :active          then ancestry.include?(p) ? 1 : 0
+             when Numeric, /^\d+$/ then options[:recurse].to_i
+             else 0
+             end
+             
+             if recurse > 0 && p.level == level && !p.root?
+               unless (child = p.children.first).nil?
+                 link << bootstrap_nav(child, options.merge(:level => level + 1, :recurse => recurse - 1), &block)
+               end
+             end
+             
+            # if options[:full] && p.children
+            #    p.children.each do |child|
+            #      link << navigation_bootstrap(child, options.merge(:full => true), &block)
+            #    end
+            #  end
+             
+            li_options = ({}).tap do |o|
+              o[:class] = [ p.root? ? "home" : p.slug ].tap do |classes|
+                classes << "active" if p == page || (active.index(p).to_i > 0 && p.level == level)
+                classes << "first"  if i == 0
+                classes << "last"   if i == siblings.length - 1
+                classes << "dropdown" if p.children.length > 0
+              end.join(" ")
+            end
+            
+            
+            str << content_tag(:li, link.html_safe, li_options)
+          end
+        end
+        
+        # Opening HTML for Twitter Bootstrap Navigation
+        str << '</ul>'
+        
+      end
+      # content_tag :div, contents.html_safe, options.except(:level, :recurse).reverse_merge(:class => "#{levels.invert[level] || "level-#{level}"} navigation")
+      contents.html_safe
+      
+    end
+    
+    
   end
 end
--- app/models/breeze/content/mixins/permalinks.rb
+++ app/models/breeze/content/mixins/permalinks.rb
@@ -7,6 +7,7 @@ module Breeze
           base.field :slug
 
           base.before_validation :fill_in_slug_and_permalink
+          base.after_save :update_child_permalinks
           base.validates_format_of :permalink, :with => /^(\/|(\/[\w\-]+)+)$/, :message => "must contain only letters, numbers, underscores or dashes"
           base.validates_uniqueness_of :permalink
           base.index :permalink
@@ -25,7 +26,21 @@ module Breeze
         def level
           permalink.count("/")
         end
-
+        
+        # This is a temporary hack until I've discussed with Blair whether we should unprotect regenerate_permalink!
+        # TODO: Remove regnerate_permalink from protected?
+        def regenerate_permalink_foo
+          self.regenerate_permalink!
+        end
+        
+        # When a permalink changes, permalinks for child pages also need to be updated
+        def update_child_permalinks
+          self.children.each do |child|
+            child.regenerate_permalink_foo
+            child.save!
+          end
+        end
+        
       protected
         def fill_in_slug_and_permalink
           self.slug = self.title.parameterize.gsub(/(^[\-]+|[-]+$)/, "") if self.slug.blank? && respond_to?(:title) && !self.title.blank?
@@ -37,6 +52,7 @@ module Breeze
         end
         
         def regenerate_permalink!
+          
           self.permalink = if respond_to?(:parent)
             if parent.nil?
               "/"
@@ -46,6 +62,7 @@ module Breeze
           else
             "/#{slug}"
           end
+                    
         end
       end
     end
