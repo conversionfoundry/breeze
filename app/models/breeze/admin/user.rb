d3492f531230386cfadfd4f47a92d5eb7413835e
Alban Diguer <albandiguer@Albans-iMac.local>
2012-09-04 13:01:48 +1200

    Include documentation Restraint the scope of some methods to private

--- app/models/breeze/admin/user.rb
+++ app/models/breeze/admin/user.rb
@@ -1,7 +1,8 @@
 require 'set'
-
 module Breeze
   module Admin
+    ##
+    # This class represents any user interacting on breeze. It embeds its logic in the model, connectors for roles and email triggering.
     class User
       include Mongoid::Document
       extend ActiveSupport::Memoizable
@@ -21,38 +22,53 @@ module Breeze
     
       after_create :schedule_new_user_email
 
-
       include Mixins::Login
       
-        
+      ##
+      # Array of current available roles
       ROLES = [ :editor, :designer, :admin ]
       
+      ##
+      # :method: can?
+      # Check user permission (in a Cancan fashion)
+
+      ##
+      # :method: cannot?
+      # Negation of #can? method 
+      delegate :can?, :cannot?, :to => :ability
+
+      ## 
+      # Returns a formated user name, this method is aliased by #to_s
       def name
-        @name ||= display_name.blank? ? [ first_name, last_name ].compact.join(" ") : display_name
+        @name ||= display_name.blank? ? [ first_name, last_name ].compact.map(&:humanize).shelljoin : display_name
       end
       alias_method :to_s, :name
       
+      ##
+      # Returns true if the user own the +sym+ role
       def role?(sym)
         roles.include? sym.to_sym
       end
-      
+     
+      ##
+      # Sets roles to the user given an array of symbols
+      #
+      # Current roles available are get_constant(ROLES)
       def roles=(values)
         write_attribute :roles, Array(values).flatten.reject(&:blank?).uniq.map(&:to_sym)
       end
       
-      def ability
-        @ability ||= Ability.new(self)
-      end
-      delegate :can?, :cannot?, :to => :ability
 
-      def method_missing(sym, *args)
-        if sym.to_s =~ /^(\w+)\?$/ && ROLES.include?($1.to_sym)
-          roles.include? $1.to_sym
-        else
-          super
-        end
-      end
-      
+
+      ## 
+      # Returns the hash of current available roles 
+      #
+      # Example
+      #   {
+      #     :admin => "Administrator",
+      #     :designer => "Designer",
+      #     :editor => "Editor"
+      #   }
       def self.roles
         @_roles ||= ({}).tap do |hash|
           ROLES.each do |role|
@@ -61,24 +77,46 @@ module Breeze
         end
       end
       
+      ##
+      # Yield the block given switching the context user @_user temporarily
       def self.with_user(user)
         old_user, @_user = @_user, user
         yield
         @_user = old_user
       end
       
+      ##
+      # Returns the context user
       def self.current
         @_user
       end
     
-      def deliver_new_user_email!
-        Breeze::Admin::UserAccountMailer.new_user_account_notification(self).deliver
-      end
-      
-
     protected
+      ##
+      # Add the new user email into the queue of background tasks 
       def schedule_new_user_email
         Breeze.queue self, :deliver_new_user_email!
+      end
+
+    private
+      def method_missing(sym, *args)
+        if sym.to_s =~ /^(\w+)\?$/ && ROLES.include?($1.to_sym)
+          roles.include? $1.to_sym
+        else
+          super
+        end
+      end
+      
+      ##
+      # Returns an instance of Admin::Ability for self 
+      def ability
+        @ability ||= Ability.new(self)
+      end
+
+      ## 
+      # Delivers new user email
+      def deliver_new_user_email!
+        Breeze::Admin::UserAccountMailer.new_user_account_notification(self).deliver
       end
     end
   end
