913d71fd2a9f8e430ebaa2d7ac87a036fbf586ed
Alban Diguer <alban@leftclick.com>
2012-09-17 09:36:05 +1200

    preprend pre on versionning

--- lib/breeze/version.rb
+++ lib/breeze/version.rb
@@ -2,9 +2,8 @@ module Breeze
 	MAJOR = 1		# increment for new backwards incompatible changes
   MINOR = 0		# increment for new backward-compatible functionality
   PATCH = 0		# increment for backwards-compaitble bug fixes
-  STATE = :pre # (nil|:pre|:alpha|:a) or every alphanumeric tag you'd like to append to your version.
+  PRE = "alpha" # (nil|pre|alpha|a|beta) or every alphanumeric tag you'd like to append to your version.
 
-  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}"
-  VERSION = [VERSION, STATE.to_s].join(".") unless STATE.nil?
+  VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join(".") 
 end
 
