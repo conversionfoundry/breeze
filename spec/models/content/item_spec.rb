b7b73a15fbf193648f7e2e967e14c2b5efc68ccc
Alban Diguer <alban@leftclick.com>
2012-10-24 17:11:06 +1300

    Typo.

--- spec/models/content/item_spec.rb
+++ spec/models/content/item_spec.rb
@@ -29,7 +29,7 @@ describe Breeze::Content::Item do
       end
     end
     context "query = 'foo labories'" do
-      before { @results = Breeze::Content::Item.search_for_text("foo labories"), :class => Breeze::Content::Mixins::Placeable }
+      before { @results = Breeze::Content::Item.search_for_text("foo labories", :class => Breeze::Content::Mixins::Placeable) }
 
       it "finds related items" do 
         @results.should include(item_1, item_3)
