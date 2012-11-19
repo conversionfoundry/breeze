ba48dfbca2eab241df6c204db6048cacb1fa0e80
Isaac Freeman <isaac@leftclick.com>
2012-11-20 09:54:26 +1300

    Trimmed commented code from page_view

--- app/models/breeze/content/page_view.rb
+++ app/models/breeze/content/page_view.rb
@@ -24,7 +24,6 @@ module Breeze
             // Insert content editor javascripts
             document.write('<script type="text/javascript" src="#{ActionController::Base.helpers.asset_path 'breeze/front.js'}"></s' + 'cript>');
 
-            // TODO: I'm not sure what the purpose of the following is - Isaac
             document.write('<script type="text/javascript" defer="defer">$(function() {');
             document.write('$("body").breeze({ page_id:"#{content.id}", view:"#{self.name}", views:#{(content.views.empty? ? %w(default) : content.views.map(&:name)).inspect}, template:"#{content.template}", templates:#{Breeze::Theming::Theme.available_templates.inspect} });');
             #{"document.write('$(\"#breeze-template-chooser\").val(\"#{content.template}\")');" if content.template?}
@@ -36,27 +35,3 @@ module Breeze
   end
 end
 
-
-
-      #def editor_html
-        #<<-HTML
-          #<script type="text/javascript">
-            #// Breeze content editing
-            #if (typeof(jQuery) == 'undefined') { document.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></s' + 'cript>'); }
-            #if (typeof(jQuery) == 'undefined' || typeof(jQuery.ui) == 'undefined') { document.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js"></s' + 'cript>'); }
-
-            #document.write('<script type="text/javascript" src="/assets/breeze/front.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/javascripts/rails.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/javascripts/marquess/marquess.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/editor/editor.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/javascripts/assets.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/javascripts/jcrop/js/jquery.Jcrop.min.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/javascripts/uploadify/swfobject.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" src="/breeze/javascripts/uploadify/uploadify.js"></s' + 'cript>');
-            #document.write('<script type="text/javascript" defer="defer">$(function() {');
-            #document.write('$("body").breeze({ page_id:"#{content.id}", view:"#{self.name}", views:#{(content.views.empty? ? %w(default) : content.views.map(&:name)).inspect}, template:"#{content.template}", templates:#{Breeze::Theming::Theme.available_templates.inspect} });');
-            ##{"document.write('$(\"#breeze-template-chooser\").val(\"#{content.template}\")');" if content.template?}
-            #document.write('});</s' + 'cript>');
-          #</script>
-        #HTML
-      #end
