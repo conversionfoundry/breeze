a746dd5fd2d5be1def1d07e118db8bdd058148f4
Alban Diguer <alban@leftclick.com>
2012-11-19 13:28:41 +1300

    Refact admin pages loaded objects.

--- app/helpers/breeze/admin/pages_helper.rb
+++ app/helpers/breeze/admin/pages_helper.rb
@@ -2,14 +2,13 @@ module Breeze
   module Admin
     module PagesHelper
       def page_tree_node(page)
-        children = pages.select { |p| p.parent_id == page.id }
         contents = link_to content_tag(:ins, "", :class => 'icon icon-' + page.class.name.demodulize.downcase) + (page.root? ? "Home" : page.title), page.permalink, :title => page.title
-        contents << content_tag(:ul, render(:partial => "breeze/admin/pages/page", :collection => children)) unless children.empty?
+        contents << content_tag(:ul, render(:partial => "breeze/admin/pages/page", :collection => page.children)) unless page.children.empty?
         content_class = [ page.class.name.demodulize.underscore ]
-        if page.root? || (@page && @page.permalink.starts_with?(page.permalink + "/"))
+        if page.root? 
           content_class << 'open'
         else
-          content_class << 'closed' unless children.empty?
+          content_class << 'closed' unless page.children.empty?
         end
         content_class << 'hidden' unless page.show_in_navigation?
         content_tag :li, contents.html_safe, :class => content_class.compact.join(" "), :id => "page_#{page.id}", :rel => (page.root? ? :root : :page)
