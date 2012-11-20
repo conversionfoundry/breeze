d5677df0baee6a057bfa835cf75dc3268e10fa4f
Isaac Freeman <isaac@leftclick.com>
2012-11-20 13:14:48 +1300

    Merged front.js into breeze.js

--- app/assets/javascripts/breeze/breeze.js
+++ app/assets/javascripts/breeze/breeze.js
@@ -1,19 +1,28 @@
 /*
- *= require jquery
+
+ * Third-party frameworks, used by plugins below
+ *= require jquery.min
  *= require jquery-ui
  *= require jquery_ujs
- *= require ./cookies
- *= require ./hotkeys
- *= require ./admin
- *= require ./jstree/jstree
- *= require ./jstree/plugins/jquery.tree.contextmenu
- *= require ./pages
- *= require ./custom_types
- *= require ./users
- *= require ./assets
- *= require ./jcrop/js/jquery.Jcrop.min
- *= require ./uploadify/swfobject
- *= require ./uploadify/uploadify
- *= require ./themes
- *= require ./codemirror/js/codemirror
+
+ * Third-party feature plugins located in vendor/javascripts
+ *= require codemirror/js/codemirror
+ *= require cookies
+ *= require hotkeys
+ *= require jcrop/js/jquery.Jcrop.min
+ *= require jstree/jstree
+ *= require jstree/plugins/jquery.tree.contextmenu
+ *= require marquess/marquess
+ *= require uploadify/swfobject
+ *= require uploadify/uploadify
+
+ * Breeze JavaScript
+ *= require ./breeze_admin
+ *= require ./breeze_admin_custom_types
+ *= require ./breeze_admin_pages
+ *= require ./breeze_admin_themes
+ *= require ./breeze_admin_users
+ *= require ./breeze_assets
+ *= require ./breeze_front_editor
+
 */
--- app/assets/javascripts/breeze/front.js
+++
@@ -1,9 +0,0 @@
-/*
- *= require jquery_ujs
- *= require ./marquess/marquess
- *= require ./editor/editor
- *= require ./assets
- *= require ./jcrop/js/jquery.Jcrop.min
- *= require ./uploadify/swfobject
- *= require ./uploadify/uploadify
-*/
