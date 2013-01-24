c182ff49f789fe19f5eff201237c2c7a48acd928
Alban Diguer <alban.diguer@gmail.com>
2013-01-25 12:48:34 +1300

    Update spec dependency related to the version change.

--- lib/breeze/version.rb
+++ lib/breeze/version.rb
@@ -1,5 +1,5 @@
 module Breeze
-	MAJOR = 1		# increment for new backwards incompatible changes
+  MAJOR = 1		# increment for new backwards incompatible changes
   MINOR = 1		# increment for new backward-compatible functionality
   PATCH = 0		# increment for backwards-compaitble bug fixes
   PRE = nil # (nil|pre|alpha|a|beta) or every alphanumeric tag you'd like to append to your version.
