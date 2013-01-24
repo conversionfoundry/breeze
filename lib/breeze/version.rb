fe02172872bddc7773652a47495f84817d1ebffd
Alban Diguer <alban.diguer@gmail.com>
2013-01-25 12:58:15 +1300

    Rollback version number incompatible with current branch name on github.

--- lib/breeze/version.rb
+++ lib/breeze/version.rb
@@ -1,6 +1,6 @@
 module Breeze
   MAJOR = 1		# increment for new backwards incompatible changes
-  MINOR = 1		# increment for new backward-compatible functionality
+  MINOR = 0		# increment for new backward-compatible functionality
   PATCH = 0		# increment for backwards-compaitble bug fixes
   PRE = nil # (nil|pre|alpha|a|beta) or every alphanumeric tag you'd like to append to your version.
 
