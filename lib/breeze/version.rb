ea906b593cbb67e4704618554b08dccea9c55c20
Isaac Freeman <isaac@leftclick.com>
2013-01-29 10:05:42 +1300

    Reverted to version 1.0.0

--- lib/breeze/version.rb
+++ lib/breeze/version.rb
@@ -1,7 +1,7 @@
 module Breeze
 	MAJOR = 1		# increment for new backwards incompatible changes
-  MINOR = 1		# increment for new backward-compatible functionality
-  PATCH = 1		# increment for backwards-compaitble bug fixes
+  MINOR = 0		# increment for new backward-compatible functionality
+  PATCH = 0		# increment for backwards-compaitble bug fixes
 
   PRE = nil # (nil|pre|alpha|a|beta) or every alphanumeric tag you'd like to append to your version.
 
