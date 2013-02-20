926d234b62a161f3fd8ba51f0d9c85392aa230a1
Alban Diguer <alban.diguer@gmail.com>
2013-02-20 23:53:58 +1300

    Rewrite content controller.

--- app/controllers/breeze/contents_controller.rb
+++ app/controllers/breeze/contents_controller.rb
@@ -3,33 +3,27 @@ module Breeze
 
     # Serves pages 
     def show
-      @path = "/" + Array(params[:path]).join("/")
-      if @path != "/" && file_path = Breeze::Theming::Theme.file(@path)
-        request.format = File.extname(file_path)[1..-1]
-        response.etag = [ file_path, File.mtime(file_path) ]
-        if request.fresh? response
-          head :not_modified
-        else
-          send_file file_path, 
-            :type => Mime[File.extname(file_path)[1..-1]].to_s, 
-            :disposition => "inline"
-        end
-      else
-        @path.sub!(/\.([^\.]*)$/) { request.format = $1; "" }
-        page = Breeze::Content[@path] or raise Breeze::Errors::NotFound, request
-
-        template = page.template || :default
-        locals = { 
-          page: page,
-          template: template
-        }
-        render file: 'vendor/themes/template/layouts/page.html.erb', locals: locals
-      end
+      # if @path != "/" && file_path = Breeze::Theming::Theme.file(@path)
+      #   request.format = File.extname(file_path)[1..-1]
+      #   response.etag = [ file_path, File.mtime(file_path) ]
+      #   if request.fresh? response
+      #     head :not_modified
+      #   else
+      #     send_file file_path, 
+      #       :type => Mime[File.extname(file_path)[1..-1]].to_s, 
+      #       :disposition => "inline"
+      #   end
+      # else
+        # @path.sub!(/\.([^\.]*)$/) { request.format = $1; "" }
+      path = "/" << params[:permalink].to_s
+      page = Breeze::Content[path] or raise Breeze::Errors::NotFound, request
+      locals = { 
+        page: page,
+        template: page.template || 'home'
+      }
+      render file: 'vendor/themes/template/layouts/page', locals: locals
+      # end
     end
-
-  private
-
-    
 
   end
 end
