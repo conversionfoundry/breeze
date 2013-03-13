de8d3017798c3104b5975f4598deda33c6f16841
Alban Diguer <alban.diguer@gmail.com>
2013-03-14 10:49:52 +1300

    Clean pages#new.

--- spec/features/admin/manage_pages_spec.rb
+++ spec/features/admin/manage_pages_spec.rb
@@ -1,27 +1,44 @@
 require 'spec_helper'
 
 feature "Manage pages in Admin panel" do
-  let!(:p) { Fabricate :page }
+  let!(:p) { Fabricate :page } # Homepage
   # let!(:p2) { Fabricate :page }
 
   background do
     sign_in # see session helper
   end
 
-
   scenario "create a page", focus: true do
     visit admin_pages_path
     click_link "New page"
+    fill_in("page_title", with: "Landing page")
+    save_and_open_page
+    click_button "OK"
+    within "#pages" do
+      expect(page).to have_content("Landing page")
+    end
   end
 
-  scenario "list all the pages" do
+  scenario "list all pages" do
     visit admin_pages_path
     expect(page).to have_content("Home")
     # expect(page).to have_content(p2.permalink)
   end
 
+  scenario "view a page" do
+    #TODO
+  end
+
+  scenario "edit a page" do
+    #TODO
+  end
+
   scenario "duplicate a page", js: :true do
-    visit admin_pages_path
+    #TODO
+  end
+
+  scenario "delete a page" do
+    #TODO
   end
 
 end
