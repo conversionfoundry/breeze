4b92198ff911e9e0f460cb57bdfbb6df86e84262
Alban Diguer <alban.diguer@gmail.com>
2013-03-28 17:14:50 +1300

    Clean front end edition spec.

--- spec/features/front_end_editor_spec.rb
+++ spec/features/front_end_editor_spec.rb
@@ -1,37 +1,20 @@
 require 'spec_helper'
 
-describe "Front End Editor", :type => :request do
-
-  before :each do
+feature "Front End Edition", :type => :request do
+  background do
+    Fabricate(:page)
   end
 
-  context "User logged in" do
-    before :each do
-      user = Fabricate :user
-      page.driver.header("USER_AGENT", "capybara")
-      visit "/admin"
-      within "#sign_in #new_admin" do
-        fill_in 'Email', :with => user.email
-        fill_in 'Password', :with => user.password
-        click_button 'Sign in'
-      end
-    end
-
-    it "Inserts editing controls into the page" do
-      home_page = Fabricate :page
-
-      visit "/"
-      page.body.should include('<body>')
-      page.body.should include("<div class='editor-panel'>")
-    end
+  scenario "as an editor it inserts editing controls into the page" do
+    sign_in
+    visit root_path
+    page.body.should include('<body>')
+    page.body.should include("<div class='editor-panel'>")
   end
 
-  context "No user logged in" do
-    it "Doesn't insert editing controls into the page" do
-      home_page = Fabricate :page
-      visit "/"
-      page.body.should include('<body>')
-      page.body.should_not include("<div class='editor-panel'>")
-    end
+  scenario "as a visitor it doesn't insert editing controls into the page" do
+    visit root_path
+    page.body.should include('<body>')
+    page.body.should_not include("<div class='editor-panel'>")
   end
 end
