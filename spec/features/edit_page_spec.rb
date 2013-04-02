9ecb9626b3656187596de912c99a82538aa56b73
Alban Diguer <alban.diguer@gmail.com>
2013-04-02 15:55:21 +1300

    Start to spec page edition.

--- spec/features/edit_page_spec.rb
+++ spec/features/edit_page_spec.rb
@@ -3,11 +3,15 @@ require 'spec_helper'
 feature 'Edit a page in the front end' do
 
   background do
+    Fabricate :page
     sign_in
   end
   
-  scenario "adds some text in the first region and saves" do
+  scenario "adds some text in the first region and saves", js: true do
     visit root_path
+    # require 'pry'; binding.pry
+    toggle_editor
+    page_header_region.click_link('+')
   end
 
 end
