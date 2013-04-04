da6a32f6805f8accf8db834e6fc35714f1aac051
Alban Diguer <alban.diguer@gmail.com>
2013-04-04 15:50:20 +1300

    Clean file locations.

--- spec/features/edit_page_spec.rb
+++ spec/features/edit_page_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 feature 'Edit a page in the front end' do
+  let!(:p) { Fabricate :page } #homepage
 
   background do
     sign_in
@@ -8,6 +9,7 @@ feature 'Edit a page in the front end' do
   
   scenario "adds some text in the first region and saves" do
     visit root_path
+
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ Spork.prefork do
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f }
-  Dir[Rails.root.join("spec/features/step/**/*.rb")].each {|f| require f }
+  Dir[Rails.root.join("spec/features/steps/**/*.rb")].each {|f| require f }
 
   RSpec.configure do |config|
     #
